67345aed26c9d7d94a7e6957cb70bf79a5b5e67b
Pallavi Agrawal <90273288+Pallavi689@users.noreply.github.com>
2023-07-01 11:31:07 +0530

    Add files via upload

---
+++ crud/sqlupdate.php
@@ -0,0 +1,13 @@
+<?php 
+ $s_idd = $_POST['sid'];
+ $s_name = $_POST['name'];
+ $s_address = $_POST['address'];
+ $s_phone = $_POST['phone'];
+ $s_course = $_POST['course'];
+ $s_rono = $_POST['ro_no'];
+
+$data = mysqli_connect("localhost","root","","crud") or die("not connect");
+$sql = " UPDATE student SET s_name = '{$s_name}',ro_no = '{$s_rono}', s_address = '{$s_address}' , phone = '{$s_phone}',course = '{$s_course}' WHERE s_id = '{$s_idd}'";
+$result=mysqli_query($data,$sql) or die("query not set");
+header("Location: http://localhost/crud/show_table.php");
+?>
